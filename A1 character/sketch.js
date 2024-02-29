4cf01e1c2675edf6038dc98af04f4fa29a0c088d
fasty2 <olusayo.olotu@gmail.com>
2024-02-29 11:50:28 -0600

    created first project

---
+++ A1 character/sketch.js
@@ -0,0 +1,18 @@
+// Project Title
+// Your Name sayo
+// Date 19n feb 2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  circle(width/2, height/2, 100);
+  fill(255)
+  rect()
+}
