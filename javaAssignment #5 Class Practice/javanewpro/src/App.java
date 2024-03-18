ee5f99e77c4b0dd7aace631718b8d86bf34b5021
fasty2 <olusayo.olotu@gmail.com>
2024-03-18 11:23:59 -0600

    Assignment #5 Class Practice
    did the requirments for in class practice

---
+++ javaAssignment #5 Class Practice/javanewpro/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("My name is Sayo"); 
+        System.out.println("name of project \\ Assignment #5 Class Practice");
+        System.out.println("3\\18\\2024");
+    }
+}
