dad3aaeaaeb625d2e3ccdcad251600467f3fd78d
fasty2 <olusayo.olotu@gmail.com>
2024-03-27 12:08:00 -0600

    assinment 5
    used system out put to display text

--- javaAssignment #5 Class Practice/javanewpro/src/App.java
+++ javaAssignment #5 Class Practice/javanewpro/src/App.java
@@ -7,11 +7,11 @@
 public class
  App {
     public static void main(String[] args) throws Exception {
-        System.out.println("My name is Sayo"); 
+        System.out.println("My Name is (sayo) and my favourite food is (shawama).\r\n" + //
+                        "Java has all sorta of weird \"quirks\" which make displaying text a bit more challenging.\r\n" + //
+                        "Sometimes characters like parentheses (sometimes called brackets) don't work correctly.\r\n" + //
+                        "Escape sequences require us to use a backslash '\\' in order to output these special characters.My name is Sayo"); 
         System.out.println("name of project \\ Assignment #5 Class Practice");
         System.out.println("3\\18\\2024");
     }
 }
-double y = 4.0;
-int x = y;
-System.out.println(x);
