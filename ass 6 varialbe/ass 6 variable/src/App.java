3f6bc2b993a956e438b6e8f2f2be4ec5fe155667
fasty2 <olusayo.olotu@gmail.com>
2024-04-10 10:51:36 -0600

    assiment 6
    variable

---
+++ ass 6 varialbe/ass 6 variable/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args)   
+    {
+    
+     double money = 0.75;
+     System.out.println(money);
+     int amount =(int)( 4.75 / money ) ;
+     System.out.println(amount);
+     
+
+}
+} 
+// write the Write the code to print the number of chicken wings 
+//you can buy if you have $4.75 and they cost $0.75 each. 
+//Remember that you can’t buy part of a wing.
+
+   //    GitHub 
+//student name: sayo 
+//date 4/9/24
