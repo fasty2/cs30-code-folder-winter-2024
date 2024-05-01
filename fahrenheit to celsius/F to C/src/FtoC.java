27ba42727b2b7d7452514e9af24b6aeb0a37a82f
fasty2 <olusayo.olotu@gmail.com>
2024-05-01 12:13:09 -0600

    temperature converter
    done better with calling from another page to anpther

--- fahrenheit to celsius/F to C/src/FtoC.java
+++ fahrenheit to celsius/F to C/src/FtoC.java
@@ -2,18 +2,11 @@
 // sayo
 // period 2
 
-import java.util.Scanner;
 
 public class FtoC {
-    public static void converter() {
-        Scanner keyboard = new Scanner(System.in);
-
-        System.out.println("What is the temp");
-        double temp2 = keyboard.nextDouble();
-
-        
+    public static void converter(double temp2) {
           
         temp2 = (temp2 - 32)*5/9;
-        System.out.println("ferinhiht temp is " + temp2+"C");
+        System.out.println("celcius temp is " + temp2+"C");
     }
 }
--- fahrenheit to celsius/F to C/src/FtoCrunner.java
+++ fahrenheit to celsius/F to C/src/FtoCrunner.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class FtoCrunner {
     public static void main(String[] args) {
-        FtoC.converter(  );
+        FtoC calc = new FtoC();
+        Scanner keyboard = new Scanner(System.in);
+
+
+        System.out.println("What is the temp");
+        double input = keyboard.nextDouble();
+        calc.converter(input);
         
+        System.out.println("What is the temp");
+         input = keyboard.nextDouble();
+        calc.converter(input);
+
+        System.out.println("What is the temp");
+         input = keyboard.nextDouble();
+        calc.converter(input);
     }
 
 }
