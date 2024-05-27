6443e724068732643357407b5d1c3e1f255814c6
fasty2 <olusayo.olotu@gmail.com>
2024-05-27 12:07:23 -0600

    project processing
    and unfinshed tri angle area

---
+++ New folder/waste/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }{
+    int x = 5, y = 6;
+    int z = x + y;
+    double a = z;
+    System.out.println(z);
+}
+}
